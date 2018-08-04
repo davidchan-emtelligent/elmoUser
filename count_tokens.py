e56c5d6b0a964284884ec6f9320024b7a6504e89
davidchan2 <david@emtelligent.com>
2018-08-04 16:32:03 -0700

    count tokens

---
+++ count_tokens.py
@@ -0,0 +1,19 @@
+import os
+
+path_data = "/shared/dropbox/ctakes_conll/tokenized_text"
+
+ds = os.listdir(path_data)
+
+n_tokens = 0
+for d in ds:
+	fs = os.listdir(os.path.join(path_data, d))
+	text = ""
+	for f in fs:
+		with open(os.path.join(path_data, d,f), "r") as fd:
+			text += fd.read().strip() + '\n'
+	tokens = text.split()
+	len1 = len(tokens)
+	print (f, len1)
+	n_tokens += len1
+
+print (n_tokens)
