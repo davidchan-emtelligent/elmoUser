252a09215131ba766c061422f2ae91c840d0f31f
davidchan2 <david@emtelligent.com>
2018-08-04 13:14:52 -0700

    update training

--- bilm/training.py
+++ bilm/training.py
@@ -783,8 +783,8 @@ def train(options, data, n_gpus, tf_save_dir, tf_log_dir,
         n_tokens_per_batch = batch_size * unroll_steps * n_gpus
         n_batches_per_epoch = int(n_train_tokens / n_tokens_per_batch)
         n_batches_total = options['n_epochs'] * n_batches_per_epoch
-        print("Training for %s epochs and %s batches" % (
-            options['n_epochs'], n_batches_total))
+        print("Training for %s epochs and %s batches, using gpu %d" % (
+            options['n_epochs'], n_batches_total, n_gpus))
 
         # get the initial lstm states
         init_state_tensors = []
@@ -880,7 +880,7 @@ def train(options, data, n_gpus, tf_save_dir, tf_log_dir,
                 summary_writer.add_summary(ret[1], batch_no)
                 dt = time.time() - t1
                 print("\rEpoch:%d/%d  batch:%d/%d  perplexity:%.2f  tokens_per_sec:%.0f  time:%.0f   "%\
-			(batch_no/n_batches_per_epoch + 1, options['n_epochs'], batch_no, \
+			(batch_no/n_batches_per_epoch, options['n_epochs'], batch_no, \
 			n_batches_total, ret[2], n_tokens_per_batch/dt, dt), end=" ")
                 sys.stdout.flush()
 
